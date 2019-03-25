969595083ee6ab066db4bcf9c22c85b6ce9ec3a7
Jordan Bieder <jordan.bieder@uliege.be>
2019-03-25 09:37:31 +0100

    Add translations files

---
+++ qdispersion/qdispersion_de.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>Mendeleev</name>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="14"/>
+        <source>Mendeleev table</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="410"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="451"/>
+        <source>Mass:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="513"/>
+        <source>Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="537"/>
+        <source>Atomic radius:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="581"/>
+        <source>Covalent radius:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.cpp" line="103"/>
+        <source>Pick a color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDispersion</name>
+    <message>
+        <location filename="qdispersion.ui" line="23"/>
+        <source>QDispersion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="75"/>
+        <source>No file loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="94"/>
+        <source>Energy</source>
+        <oldsource>Energy </oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="115"/>
+        <source>Energy origin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="172"/>
+        <source>Energy unit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="213"/>
+        <source>Ignore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="227"/>
+        <source>bands</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="253"/>
+        <source>Legend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="274"/>
+        <source>Number of segments</source>
+        <oldsource>Number of segments </oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="328"/>
+        <source>Number of points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="416"/>
+        <source>Labels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="480"/>
+        <source>Fat&amp;bands</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="492"/>
+        <source>Atoms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="502"/>
+        <source>Orbital angular momentum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="535"/>
+        <source>all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="543"/>
+        <source>Magnetic quantum number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="23"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="24"/>
+        <source>&amp;Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="25"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="26"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="65"/>
+        <source>Loading file </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="71"/>
+        <source>Ready</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="121"/>
+        <location filename="qdispersion.cpp" line="220"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="213"/>
+        <source>Plot in progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="344"/>
+        <source>Dispersion file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>atomicData</name>
+    <message>
+        <location filename="../tools/atomicdata.ui" line="61"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/atomicdata.ui" line="76"/>
+        <source>Mass</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/atomicdata.ui" line="91"/>
+        <source>radius</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/atomicdata.ui" line="106"/>
+        <source>rcov</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ qdispersion/qdispersion_fr.ts
@@ -0,0 +1,204 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>Mendeleev</name>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="14"/>
+        <source>Mendeleev table</source>
+        <translation>Tableau de Mendeleev</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="410"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="451"/>
+        <source>Mass:</source>
+        <translation>Masse:</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="513"/>
+        <source>Color:</source>
+        <translation>Couleur:</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="537"/>
+        <source>Atomic radius:</source>
+        <translation>Rayon atomique:</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.ui" line="581"/>
+        <source>Covalent radius:</source>
+        <translation>Rayon de covalence:</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/mendeleev.cpp" line="103"/>
+        <source>Pick a color</source>
+        <translation>Choisir une couleur</translation>
+    </message>
+</context>
+<context>
+    <name>QDispersion</name>
+    <message>
+        <location filename="qdispersion.ui" line="23"/>
+        <source>QDispersion</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="75"/>
+        <source>No file loaded</source>
+        <translation>Auncun fichier</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="94"/>
+        <source>Energy</source>
+        <oldsource>Energy </oldsource>
+        <translation>Energie</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="115"/>
+        <source>Energy origin</source>
+        <translation>Origine des énergies</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="172"/>
+        <source>Energy unit</source>
+        <translation>Unité des énergies</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="213"/>
+        <source>Ignore</source>
+        <translation>Ignore</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="227"/>
+        <source>bands</source>
+        <translation>bandes</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="253"/>
+        <source>Legend</source>
+        <translation>Légende</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="274"/>
+        <source>Number of segments</source>
+        <oldsource>Number of segments </oldsource>
+        <translation>Nombre de segments</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="328"/>
+        <source>Number of points</source>
+        <translation>Nombres de points</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="416"/>
+        <source>Labels</source>
+        <translation>Etiquettes</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="480"/>
+        <source>Fat&amp;bands</source>
+        <translation>Fat&amp;bands</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="492"/>
+        <source>Atoms</source>
+        <translation>Atomes</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="502"/>
+        <source>Orbital angular momentum</source>
+        <translation>Moment orbital angulaire</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="535"/>
+        <source>all</source>
+        <translation>Tous</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.ui" line="543"/>
+        <source>Magnetic quantum number</source>
+        <translation>Nombre quantique magnétique</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="23"/>
+        <source>&amp;Save</source>
+        <translation>Enregi&amp;strer</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="24"/>
+        <source>&amp;Apply</source>
+        <translation>&amp;Appliquer</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="25"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Fermer</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="26"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Ouvrir</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="65"/>
+        <source>Loading file </source>
+        <translation>Chargement du fichier </translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="71"/>
+        <source>Ready</source>
+        <translation>Prêt</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="121"/>
+        <location filename="qdispersion.cpp" line="220"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="213"/>
+        <source>Plot in progress</source>
+        <translation>Traçage en cours</translation>
+    </message>
+    <message>
+        <location filename="qdispersion.cpp" line="344"/>
+        <source>Dispersion file</source>
+        <translation>Fichier de dispersion</translation>
+    </message>
+</context>
+<context>
+    <name>atomicData</name>
+    <message>
+        <source>Form</source>
+        <translation type="obsolete">Fomulaire</translation>
+    </message>
+    <message>
+        <source>Z</source>
+        <translation type="obsolete">Z</translation>
+    </message>
+    <message>
+        <location filename="../tools/atomicdata.ui" line="61"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../tools/atomicdata.ui" line="76"/>
+        <source>Mass</source>
+        <translation>Masse</translation>
+    </message>
+    <message>
+        <location filename="../tools/atomicdata.ui" line="91"/>
+        <source>radius</source>
+        <translation>Rayon</translation>
+    </message>
+    <message>
+        <location filename="../tools/atomicdata.ui" line="106"/>
+        <source>rcov</source>
+        <translation>Covalence</translation>
+    </message>
+</context>
+</TS>
