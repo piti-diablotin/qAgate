6976264c98c9459b3aca8827b3f7fa12d8fbf938
Jordan Bieder <jordan.bieder@uliege.be>
2023-08-08 23:01:30 +0200

    Remove compile warning

--- qdos/main_qdos.cpp
+++ qdos/main_qdos.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
   Exception::setVerbosity(5);
   QString locale = QLocale::system().name().section('_', 0, 0);
   QTranslator translator;
-  translator.load(QString(":/translations/qdos_")+ locale);
+  [[maybe_unused]] auto transLoaded = translator.load(QString(":/translations/qdos_")+ locale);
   a.installTranslator(&translator);
   QDos w;
   w.show();
